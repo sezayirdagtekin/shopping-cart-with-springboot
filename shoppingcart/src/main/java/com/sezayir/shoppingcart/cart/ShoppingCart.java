169dd42d61032ee71970082651f19dce36b934a3
sezayirdagtekin <sezayir@hotmail.com>
2020-05-18 18:13:29 +0300

    start to implement parts

--- shoppingcart/src/main/java/com/sezayir/shoppingcart/cart/ShoppingCart.java
+++ shoppingcart/src/main/java/com/sezayir/shoppingcart/cart/ShoppingCart.java
@@ -2,13 +2,11 @@ package com.sezayir.shoppingcart.cart;
 
 import static com.sezayir.shoppingcart.campaign.DiscountTypeEnum.AMOUNT;
 import static com.sezayir.shoppingcart.campaign.DiscountTypeEnum.RATE;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -21,7 +19,6 @@ import com.sezayir.shoppingcart.model.Product;
 import com.sezayir.shoppingcart.model.ShoppingCartItem;
 
 /**
- * O
  * 
  * @author sezayir
  *
@@ -30,18 +27,56 @@ import com.sezayir.shoppingcart.model.ShoppingCartItem;
 public class ShoppingCart {
 
 	ShoppingCartItem shoppingCartItem;
-	private BigDecimal totalAmount;
 
-	private final List<ShoppingCartItem> items = new ArrayList<>();
+	private BigDecimal totalAmoutAfterDiscount;
+	private BigDecimal couponDiscount;
+	private BigDecimal campaignDiscount;
+	private BigDecimal deliveryCost;
 	private List<ShoppingCartItem> discountedItems;
+	private final List<ShoppingCartItem> items = new ArrayList<>();
 	private static final Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
 
 	public List<ShoppingCartItem> getItems() {
 		return items;
 	}
-
 	public List<ShoppingCartItem> getDiscountedItems() {
 		return discountedItems;
+	}
+	public ShoppingCartItem getShoppingCartItem() {
+		return shoppingCartItem;
+	}
+	public void setShoppingCartItem(ShoppingCartItem shoppingCartItem) {
+		this.shoppingCartItem = shoppingCartItem;
+	}
+	public BigDecimal getTotalAmoutAfterDiscount() {
+		return totalAmoutAfterDiscount;
+	}
+	public void setTotalAmoutAfterDiscount(BigDecimal totalAmoutAfterDiscount) {
+		this.totalAmoutAfterDiscount = totalAmoutAfterDiscount;
+	}
+	public BigDecimal getCouponDiscount() {
+		return couponDiscount;
+	}
+	public void setCouponDiscount(BigDecimal couponDiscount) {
+		this.couponDiscount = couponDiscount;
+	}
+
+	public BigDecimal getCampaignDiscount() {
+		return campaignDiscount;
+	}
+
+	public void setCampaignDiscount(BigDecimal campaignDiscount) {
+		this.campaignDiscount = campaignDiscount;
+	}
+	public BigDecimal getDeliveryCost() {
+		return deliveryCost;
+	}
+	public void setDeliveryCost(BigDecimal deliveryCost) {
+		this.deliveryCost = deliveryCost;
+	}
+
+	public void setDiscountedItems(List<ShoppingCartItem> discountedItems) {
+		this.discountedItems = discountedItems;
 	}
 
 	/**
@@ -64,12 +99,9 @@ public class ShoppingCart {
 	 * @return
 	 */
 	public List<ShoppingCartItem> applyDiscount(Campaign... campaign) {
-
 		Map<Category, Integer> map = getItems().stream().collect(
 				Collectors.groupingBy(s -> s.getProduct().getCategory(), Collectors.summingInt(s -> s.getQuantity())));
-
 		discountedItems = getItems();
-
 		for (Campaign c : campaign) {
 			Integer totalNumberOfProductInCategory = map.get(c.getCategory());
 			if (totalNumberOfProductInCategory != null && totalNumberOfProductInCategory >= c.getNumberOfItems())
@@ -79,38 +111,40 @@ public class ShoppingCart {
 							f.getProduct().setPrice(f.getProduct().getPrice().multiply(multiplierFactor(c)));
 						}
 					});
-
 				} else if (AMOUNT.equals(c.getDiscountType())) {
 					discountedItems.forEach(
 							f -> f.getProduct().setPrice(f.getProduct().getPrice().subtract(c.getDiscountValue())));
-
 				}
 		}
-
+		BigDecimal amountBeforeCampaign = getItems().stream().map(s -> s.getProduct().getPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		BigDecimal amountAfterCampaign = discountedItems.stream().map(s -> s.getProduct().getPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		setTotalAmoutAfterDiscount(amountAfterCampaign);
+		setCampaignDiscount(amountBeforeCampaign.subtract(amountAfterCampaign));
 		return discountedItems;
-
 	}
 
-	private BigDecimal multiplierFactor(Campaign c) {
-		return BigDecimal.ONE.subtract(c.getDiscountValue().divide(new BigDecimal(100.0)));
-	}
 
 	public void applyCoupon(Coupon coupon) {
-
 		List<ShoppingCartItem> cart = getDiscountedItems();
-		totalAmount = cart.stream().map(s -> s.getProduct().getPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
-		if (totalAmount.compareTo(coupon.getMinAmount()) > 0) {
-			logger.info("Before coupon applied total is:" + totalAmount);
-			totalAmount = totalAmount.multiply(BigDecimal.ONE.subtract(coupon.getDiscountRate().divide(new BigDecimal(100.0))));
-			logger.info("After coupon applied total is:" + totalAmount);
+		BigDecimal totalAmountBefore = cart.stream().map(s -> s.getProduct().getPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		if (totalAmountBefore.compareTo(coupon.getMinAmount()) > 0) {
+			logger.info("Before coupon applied total is:" + totalAmountBefore);
+			BigDecimal 	totalAmountAfter = totalAmountBefore.multiply(BigDecimal.ONE.subtract(coupon.getDiscountRate().divide(new BigDecimal(100.0))));
+			logger.info("After coupon applied total is:" + totalAmountAfter);
+			setCouponDiscount(totalAmountBefore.subtract(totalAmountAfter));
 		}
 	}
 
 	public void calculateDeliveryCost() {
-		DeliveryCostCalulator deliveryCostCalulator = new DeliveryCostCalulator(new BigDecimal(10.0), new BigDecimal(5.0),
-				new BigDecimal(2.0));
-		BigDecimal totalDeliveryCost = deliveryCostCalulator.calculate(this);
-		logger.info("Total delivery cost is:" + totalDeliveryCost);
+		DeliveryCostCalulator deliveryCostCalulator = new DeliveryCostCalulator(new BigDecimal(10.0),
+				new BigDecimal(5.0), new BigDecimal(2.0));
+		setDeliveryCost(deliveryCostCalulator.calculate(this));
+		logger.info("Total delivery cost is calculated:" + getDeliveryCost());
 	}
+	
+	private BigDecimal multiplierFactor(Campaign c) {
+		return BigDecimal.ONE.subtract(c.getDiscountValue().divide(new BigDecimal(100.0)));
+	}
+
 
 }
