85f3a566b04b141372d8e31da982c85107f33e94
sezayirdagtekin <sezayir@hotmail.com>
2020-05-18 08:47:16 +0300

    add test conroller for genareting test data

--- shoppingcart/src/main/java/com/sezayir/shoppingcart/controller/ShoppingCartController.java
+++ shoppingcart/src/main/java/com/sezayir/shoppingcart/controller/ShoppingCartController.java
@@ -1,9 +1,9 @@
 package com.sezayir.shoppingcart.controller;
 
+import static com.sezayir.shoppingcart.util.Constants.FOOD;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.sezayir.shoppingcart.campaign.Campaign;
 import com.sezayir.shoppingcart.coupon.Coupon;
 import com.sezayir.shoppingcart.model.Category;
@@ -19,9 +18,7 @@ import com.sezayir.shoppingcart.model.Product;
 import com.sezayir.shoppingcart.model.ShoppingCartItem;
 import com.sezayir.shoppingcart.services.CampaignService;
 import com.sezayir.shoppingcart.services.CouponService;
-import com.sezayir.shoppingcart.services.ShoppingService;
-import static com.sezayir.shoppingcart.util.Constants.ELECTRONIC;
-import static com.sezayir.shoppingcart.util.Constants.BOOK;;
+import com.sezayir.shoppingcart.services.ShoppingService;;
 
 @RestController
 @RequestMapping("/basket")
@@ -46,20 +43,12 @@ public class ShoppingCartController {
 	}
 
 	@RequestMapping("/add")
-	public ResponseEntity<String> addBasketItems() {
+	public ResponseEntity<Product> addBasketItems() {
 		logger.info("addBasketItems end point is called...");
-		Category category = new Category(ELECTRONIC, null);
-		Category category2 = new Category(BOOK, null);
-		Product product1 = new Product("Printer", new BigDecimal(250.00), category);
-		shoppingService.addItems(product1, 5);
-		
-		Product product2 = new Product("Samsung TV", new BigDecimal(500.00), category);
-		shoppingService.addItems(product2, 4);
-		
-		Product product3 = new Product("Java Fundamentels", new BigDecimal(100.00), category2);
-		shoppingService.addItems(product3, 1);
-		
-		return new ResponseEntity<String>(HttpStatus.OK);
+		Category category = new Category(FOOD, null);
+		Product product = new Product("Nutella", new BigDecimal(40.00), category);
+		shoppingService.addItems(product, 5);
+		return new ResponseEntity<Product>(product,HttpStatus.OK);
 	}
 
 	
---
+++ shoppingcart/src/main/java/com/sezayir/shoppingcart/controller/TestController.java
@@ -0,0 +1,73 @@
+package com.sezayir.shoppingcart.controller;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.sezayir.shoppingcart.campaign.Campaign;
+import com.sezayir.shoppingcart.coupon.Coupon;
+import com.sezayir.shoppingcart.model.Category;
+import com.sezayir.shoppingcart.model.Product;
+import com.sezayir.shoppingcart.model.ShoppingCartItem;
+import com.sezayir.shoppingcart.services.CampaignService;
+import com.sezayir.shoppingcart.services.CouponService;
+import com.sezayir.shoppingcart.services.ShoppingService;
+import static com.sezayir.shoppingcart.util.Constants.ELECTRONIC;
+import static com.sezayir.shoppingcart.util.Constants.BOOK;;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	@Autowired
+	ShoppingService shoppingService;
+
+	@Autowired
+	CampaignService campaignService;
+	
+	@Autowired
+	CouponService couponService;
+
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
+	@RequestMapping("/generate")
+	public ResponseEntity<String> generate() {
+		logger.info("generare end point is called...");
+		//Create Category
+		Category category1 = new Category(ELECTRONIC, null);
+		Category category2 = new Category(BOOK, null);
+		
+		Product product1 = new Product("Printer", new BigDecimal(250.00), category1);
+		shoppingService.addItems(product1, 5);
+		
+		Product product2 = new Product("Samsung TV", new BigDecimal(500.00), category1);
+		shoppingService.addItems(product2, 4);	
+		Product product3 = new Product("Java Fundamentels", new BigDecimal(100.00), category2);
+		shoppingService.addItems(product3, 1);
+		
+		//Create campaigns
+		campaignService.createCampaigns();
+		//Get campaigns
+		HashMap<String, Campaign> campaignMap = campaignService.getCamapigns();
+		Campaign campaign1 = campaignMap.get("DSC%20");
+		Campaign campaign2 = campaignMap.get("DSC%50");
+		List<ShoppingCartItem> discount=shoppingService.applyDiscount(campaign1, campaign2);
+
+		//Apply coupon
+		Coupon coupon=couponService.getCoupon();
+		shoppingService.applyCoupon(coupon);
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+
+
+
+
+}
